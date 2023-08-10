30f569beaa1ee2fb0882b372fb3abe91c5113d10
Albin Kurian <Albin.Kurian@asl.mum.sst>
2023-08-11 02:41:19 +0530

    first commit

---
+++ StudentApp/Model/Student.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace StudentApp.Model
+{
+    internal class Student
+    {
+        private int rollno;
+        private string name;
+        private double cgpa;
+
+        public int RollNo
+        {
+            get { return rollno; }
+            set
+            {
+                if (ValidateRollNo(value))
+                { 
+                    rollno = value;
+                }
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = ValidateName(value);
+            }
+        }
+
+        public double CGPA
+        {
+            get { return cgpa; }
+            set
+            {
+                if (ValidateCgpa(value))
+                {
+                    cgpa = value;
+                }
+            }
+        }
+
+
+        public bool ValidateRollNo(int value)
+        {
+            if (value > 0)
+                return true;
+            else
+                Console.WriteLine("Roll number must be a positive value.");
+            return false;
+        }
+
+        public string ValidateName(string value)
+        {
+            if (value.Length >= 5)
+                return value;
+            return "";
+
+        }
+        public bool ValidateCgpa(double value)
+        {
+            if (value >= 1 && value <= 10)
+                return true;
+            else
+                Console.WriteLine(" CGPA must be in the range 1 to 10.");
+            return false;
+
+        }
+
+
+
+
+        public double CalculatePercentage()
+        {
+            double conversionFactor = 9.5;
+            double conversionOffset = 0;
+
+            double percentage = cgpa * conversionFactor + conversionOffset;
+
+            return percentage; 
+        }
+    }
+}
---
+++ StudentApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using StudentApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp
+{
+    internal class StudentApp
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+ 
+            Console.Write("Enter Roll NUmber: ");
+            student1.RollNo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Student Name: ");
+            student1.Name = Console.ReadLine();
+
+            Console.Write("Enter Student CGPA: ");
+            student1.CGPA = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Student 1: RollNo = {0}, Name = {1}, CGPA = {2}", student1.RollNo, student1.Name, student1.CGPA);
+
+            Console.WriteLine("Student 1 Percentage: {0}%", student1.CalculatePercentage());
+
+            // This will print an error message.
+            //Student student2 = new Student();
+            //student2.RollNo = -133; 
+            //student2.Name = "Alb";
+            //student2.CGPA = 7.6;     // This will print an error message.
+            //Console.WriteLine("Student 2: RollNo = {0}, Name = {1}, CGPA = {2}", student2.RollNo, student2.Name, student2.CGPA);
+            //Console.WriteLine("Student 2 Percentage: {0}%", student2.CalculatePercentage());
+        }
+    }
+}
